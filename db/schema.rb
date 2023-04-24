855b9cfd9cb41a6073b41124864f5919d66f0cc6
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2023-04-23 22:01:53 -0300

    created the login page end db data

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_193201) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.string "phone", null: false
+    t.string "name"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
