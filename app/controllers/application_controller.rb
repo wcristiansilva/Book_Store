bb2a693913119640e67ab292939b1da163ddef67
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2023-06-04 22:19:13 -0300

    #

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-    layout :layout_by_resource
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-    private
+    protected
 
-    def layout_by_resource
-        if devise_controller?
-            "devise"
-        else
-            "application"
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u| 
+            u.permit(:name, :email, :password, :password_confirmation, :phone)
         end
     end
 end
