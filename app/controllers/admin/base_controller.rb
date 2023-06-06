5eb18ddf8ca53ed2c398d0ca4774696575159b9d
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2023-06-05 21:11:03 -0300

    configured the session of admin/user painel

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,5 +2,12 @@ module Admin
     class BaseController < ApplicationController
         layout 'admin'
         before_action :authenticate_user!
+        before_action :only_admin!
+
+        private 
+
+        def only_admin!
+            redirect_to root_path unless current_user.admin?
+        end
     end
 end
